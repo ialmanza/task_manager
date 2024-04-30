be6adb4496f58dfbe7bf963fe50457774df2edf8
ialmanza <110355705+ialmanza@users.noreply.github.com>
2024-04-30 14:54:41 -0300

    LocalStorage funcionando

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Task } from '../models/Task';
 
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -38,6 +39,7 @@ export class TaskService {
       // Verifica si localStorage está disponible
       if (typeof localStorage !== 'undefined') {
          // Obtiene las tareas actuales del localStorage
+
          const storedTasks = this.getTasks();
 
          // Agrega la nueva tarea al array de tareas
@@ -51,14 +53,36 @@ export class TaskService {
       }
      }
 
+    // deleteTask(task: Task){
+    //   for(let i = 0; i < this.tasks.length; i++){
+    //     if(this.tasks[i] === task){
+    //       this.tasks.splice(i, 1);
+    //       localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    //     }
+    //   }
+    // }
 
-    deleteTask(task: Task){
-      for(let i = 0; i < this.tasks.length; i++){
-        if(this.tasks[i] === task){
-          this.tasks.splice(i, 1);
-          localStorage.setItem('tasks', JSON.stringify(this.tasks));
-        }
+    deleteTask(title: string) {
+      // Verifica si localStorage está disponible
+      if (typeof localStorage !== 'undefined') {
+         // Obtiene las tareas actuales del localStorage
+         const storedTasks = this.getTasks();
+
+         // Busca la tarea por su título y la elimina del array
+         const taskIndex = storedTasks.findIndex(task => task.title === title);
+         if (taskIndex !== -1) {
+           storedTasks.splice(taskIndex, 1);
+
+           // Guarda el array actualizado en el localStorage
+           localStorage.setItem('tasks', JSON.stringify(storedTasks));
+         } else {
+           console.error('La tarea con el título proporcionado no se encontró.');
+         }
+      } else {
+         // Si localStorage no está disponible, puedes manejarlo como prefieras
+         console.error('localStorage no está disponible en este entorno.');
       }
-    }
+     }
+
 
   }
